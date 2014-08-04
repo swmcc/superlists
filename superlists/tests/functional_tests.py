00ba6869ac378b9ab01450cfec898f378f4e8714
Stephen McCullough <me@swm.cc>
2014-08-04 21:05:31 +0100

    Modified the functional test

--- superlists/tests/functional_tests.py
+++ superlists/tests/functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert('Django' in browser.title)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(5)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+
+		# Go to the app
+		self.browser.get('http://localhost:8000')
+
+		# The title should have 'To-Do' in it
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		# Add an item by default
+
+		# Type in "Finish TDD next chapter"
+
+		# Hitting enter should force the page to update and the page should
+		# list the "Finish TDD next chapter"
+
+		# There is still a text box wating for another item
+
+		# Type in "Remember to upload the changed to github"
+
+		# Page updates and now both items are on the list
+
+		# Close session and come back again and you should see both items
+
+		# Everything is good
+
+		self.browser.quit()
+
+if __name__ == '__main__':
+	unittest.main()
+
